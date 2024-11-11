2b61bc822f3ca85c50e8b4328280b5f702cdcc4c
qingshan <24538474+lniche@users.noreply.github.com>
2024-11-11 23:30:05 +0800

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -31,9 +31,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	logger.Info("server start", zap.String("host", fmt.Sprintf("http://%s:%d", conf.GetString("http.host"), conf.GetInt("http.port"))))
-	logger.Info("docs addr", zap.String("addr", fmt.Sprintf("http://%s:%d/swagger-ui/index.html", conf.GetString("http.host"), conf.GetInt("http.port"))))
+	logger.Info("===============================")
+	logger.Info("Listening on", zap.String("host", fmt.Sprintf("http://%s:%d", conf.GetString("http.host"), conf.GetInt("http.port"))))
+	logger.Info("Docs addr", zap.String("addr", fmt.Sprintf("http://%s:%d/swagger-ui/index.html", conf.GetString("http.host"), conf.GetInt("http.port"))))
+	logger.Info("===============================")
 	if err = app.Run(context.Background()); err != nil {
 		panic(err)
 	}
